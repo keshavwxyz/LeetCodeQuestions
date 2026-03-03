6a928dda3b544990d9baa71218645a618b2423cb
keshavwxyz <keshavbhardwaj5354.com>
2026-03-04 02:02:18 +0530

    first question done

--- Easy/1.two-sum.cpp
+++ Easy/1.two-sum.cpp
@@ -7,8 +7,16 @@
 // @lc code=start
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        
+    vector<int> twoSum(vector<int> &nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[j] == target - nums[i]) {
+                    return {i, j};
+                }
+            }
+        }
+        // Return an empty vector if no solution is found
+        return {};
     }
 };
 // @lc code=end
